1e08ae669131bd4a5476122f9101eb1727ad0350
Yunxing Dai <yunxing@upthere.com>
2015-05-02 12:05:04 -0700

    hk mode

--- init-flycheck.el
+++ init-flycheck.el
@@ -1,8 +1,5 @@
 (when (eval-when-compile (>= emacs-major-version 24))
   (require-package 'flycheck)
-  (setq flycheck-clang-include-path (split-string "
-/Users/yunxing/source/upthere/include
-"))
   (add-hook 'after-init-hook 'global-flycheck-mode))
 
 (provide 'init-flycheck)
--- init.el
+++ init.el
@@ -24,8 +24,8 @@
 (require-package 'mwe-log-commands)
 (require-package 'go-mode)
 (setq-default tab-width 4)
-(add-hook 'go-mode-hook (lambda ()
-                          (add-hook 'before-save-hook 'gofmt-before-save)))
+;; (add-hook 'go-mode-hook (lambda ()
+;;                           (add-hook 'before-save-hook 'gofmt-before-save)))
 (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
 (require 'golint)
 
@@ -68,7 +68,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(warning-minimum-level :error))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
